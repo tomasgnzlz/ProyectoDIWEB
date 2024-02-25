1347b6b5c930d8d67539573e6b1965d3468377e9
tgonati327 <tgonati327@g.educaand.es>
2024-02-25 21:44:23 +0100

    Frame 1 listo

--- js/funciones.js
+++ js/funciones.js
@@ -4,14 +4,36 @@ $(document).ready(function(){
     var cerrarMenu = $(".menu > div.cabecera-menu > img");
 
     // Cuando se hace click sobre el icono del menu, este se desliza de izquierda a derecha
-    iconoMenu.on('click', function() {
-        menuDesplegable.addClass('menu-abierto');
+    iconoMenu.on("click", function() {
+        menuDesplegable.addClass("menu-abierto");
     });
 
-    cerrarMenu.on('click', function() {
-        menuDesplegable.removeClass('menu-abierto');
+    // Cuando se hace click sobre la flecha de volver, el menu se oculta
+    cerrarMenu.on("click", function() {
+        menuDesplegable.removeClass("menu-abierto");
     });
 
+
+    var contenido = $("div#contenido");
+    $(".boton-cookies").on("click", function() {
+        // Oculto el panel de cookies
+        $("#cookies-principal").fadeOut();
+        // quito el efecto blur
+        contenido.removeClass("blur");
+    });
+
+    // Se pone un borde a los botones cuando se esta sobre ellos
+    var botonesCookies = $(".boton-cookies");
+    botonesCookies.on({
+        mouseenter:function () {
+            $(this).css("border","solid 1px black");
+        },
+        mouseleave:function () {
+            $(this).css("border","");
+        }
+    })
+
+  
 
 
 
